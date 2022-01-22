d683788ba5d9a69da5b0ad660c19ec783c3d7798
memarino92 <memarino92@gmail.com>
2022-01-21 23:20:31 -0500

    First commit

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+import PostsCell from 'src/components/PostsCell'
+
+const HomePage = () => {
+  return (
+    <>
+      <PostsCell />
+    </>
+  )
+}
+
+export default HomePage
